ca2d22c1feb4447c84acfd542e7f280f31105819
Basilio <paledot01@gmail.com>
2023-01-05 23:29:52 -0500

    version 0.6

--- src/main/java/com/edu/cibertec/service/impl/EstadoServiceImpl.java
+++ src/main/java/com/edu/cibertec/service/impl/EstadoServiceImpl.java
@@ -3,11 +3,13 @@ package com.edu.cibertec.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.edu.cibertec.model.Estado;
 import com.edu.cibertec.repository.EstadoRepository;
 import com.edu.cibertec.service.EstadoService;
 
+@Service
 public class EstadoServiceImpl implements EstadoService{
 
 	@Autowired
